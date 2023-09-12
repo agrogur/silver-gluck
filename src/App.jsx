06e877ea1155a2aa36367c5f25b6929b0d06648b
UĞUR GÜMÜŞ <83755769+agrogur@users.noreply.github.com>
2023-09-12 13:40:18 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Uğur Gümüş",
+  title: "Junior Python Developer",
+  email: "agrogur@gmail.com",
+  gitHub: "agrogur",
+  instagram: "agrogur",
+  linkedIn: "agrogur",
+  medium: "agrogur",
+  twitter: "u47gur",
+  youTube: "agrogur",
 };
 
 const primaryColor = "#4E567E";
