783457408f3565834b25fc0251084669ca8930ca
UĞUR GÜMÜŞ <83755769+agrogur@users.noreply.github.com>
2023-09-20 13:14:42 +0300

    Update Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -67,8 +67,7 @@ const Portfolio = () => {
               height: "90%",
               width: "100%",
               objectFit: "cover",
-              animation: "1s ease-out 0s 1 slideIn",
-            }}
+              }}
             alt={imageAltText}
           />
         </div>
